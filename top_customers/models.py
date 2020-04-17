d981566d998564926c1d99dfb98b58f171f60026
viper24rus <viper24russ@gmail.com>
2020-04-17 18:21:41 +0700

    Created 'Customer' model

--- top_customers/models.py
+++ top_customers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    username = models.CharField(max_length=20, primary_key=True)
+    gems = models.TextField(default='')
+    spent_money = models.IntegerField(default=0)
+
+    def add_item(self, item):
+        '''
+        метод добавления товара в список товаров,
+        которые приобрел покупатель
+        '''
+        if self.gems.find(item) == -1:
+            if self.gems == '':
+                self.gems = item
+            else:
+                self.gems = self.gems + ',' + item
+
+    def __str__(self):
+        return self.username
